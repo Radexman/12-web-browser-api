b2c3fe32cc8fe8271f7192465d97b171902079c5
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-05-30 16:48:11 +0200

    Project Finished

--- 06-music-player-project/script.js
+++ 06-music-player-project/script.js
@@ -1,1 +1,93 @@
-//
+// ================== 06 Music Player Project ================== //
+
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+// Song titles
+const songs = ['hey', 'summer', 'ukulele'];
+
+// Keep track of song
+let songIndex = 2;
+
+// Initially load song details
+
+const loadSong = (song) => {
+	title.textContent = song;
+	audio.src = `music/${song}.mp3`;
+	cover.src = `images/${song}.jpg`;
+};
+
+loadSong(songs[songIndex]);
+
+const playSong = () => {
+	musicContainer.classList.add('play');
+	playBtn.querySelector('i.fas').classList.remove('fa-play');
+	playBtn.querySelector('i.fas').classList.add('fa-pause');
+	audio.play();
+};
+
+const pauseSong = () => {
+	musicContainer.classList.remove('play');
+	playBtn.querySelector('i.fas').classList.remove('fa-pause');
+	playBtn.querySelector('i.fas').classList.add('fa-play');
+	audio.pause();
+};
+
+const prevSong = () => {
+	songIndex--;
+
+	if (songIndex < 0) {
+		songIndex = songs.length - 1;
+	}
+
+	loadSong(songs[songIndex]);
+
+	playSong();
+};
+
+const nextSong = () => {
+	songIndex++;
+
+	if (songIndex > songs.length - 1) {
+		songIndex = 0;
+	}
+
+	loadSong(songs[songIndex]);
+
+	playSong();
+};
+
+const updateProgress = (e) => {
+	const { duration, currentTime } = e.srcElement;
+	const progresPrecent = (currentTime / duration) * 100;
+	progress.style.width = `${progresPrecent}%`;
+};
+
+function setProgress(e) {
+	const width = this.clientWidth;
+	const clickX = e.offsetX;
+	const duration = audio.duration;
+	audio.currentTime = (clickX / width) * duration;
+}
+
+playBtn.addEventListener('click', () => {
+	const isPlaying = musicContainer.classList.contains('play');
+
+	if (isPlaying) {
+		pauseSong();
+	} else {
+		playSong();
+	}
+});
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+progressContainer.addEventListener('click', setProgress);
+audio.addEventListener('timeupdate', updateProgress);
